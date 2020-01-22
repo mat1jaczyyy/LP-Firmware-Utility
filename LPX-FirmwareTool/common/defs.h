38664954b2c1a1c1074719cf1c77c77d89dbb26c
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-01-22 14:47:14 +0100

    Create LPX FirmwareTool solution

---
+++ LPX-FirmwareTool/common/defs.h
@@ -0,0 +1,12 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <string>
+
+typedef unsigned char byte;
+typedef unsigned int uint;
+
+#endif
---
+++ LPX-FirmwareTool/lpx-syxtobin/main.cpp
@@ -0,0 +1,5 @@
+#include "defs.h"
+
+int main(int argc, char** argv) {
+	return 0;
+}
