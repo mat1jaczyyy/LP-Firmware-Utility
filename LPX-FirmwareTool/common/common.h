f607743d8fd859c9bc86282fa9cfe26702ba0317
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-01-22 19:15:12 +0100

    Implement syxtobin

--- LPX-FirmwareTool/common/common.h
+++ LPX-FirmwareTool/common/common.h
@@ -33,46 +33,50 @@ const std::vector<byte> novation_header = {0x00, 0x20, 0x29, 0x00};
 const std::vector<byte> product_types = {LPX_PRODUCT_ID, LPMINIMK3_PRODUCT_ID, LPPROMK3_PRODUCT_ID};
 
 bin_t input, output;
-uint version;
+uint version, checksum, w;
+
+char default_output[4] =
+#ifdef SYXTOBIN
+	"bin"
+#else
+	#ifdef BINTOSYX
+		"syx"
+	#else 
+		#error Repacker type not defined
+	#endif
+#endif
+;
 
 int argc;
 char** argv;
 
-bool parse_args(int c, char** v) {
-	if (c < 2) {
-		fprintf(stderr, "No input file specified.\n");
+bool allocate_buffer(bin_t* buffer, int size, const char* error) {
+	buffer->size = size;
+	buffer->data = (byte*)malloc(buffer->size * sizeof(*buffer->data));
+
+	if (buffer->data == NULL) {
+		fprintf(stderr, "Failed to allocate memory for %s file.\n", error);
+
 		return false;
 	}
 
-	if (c > 3) {
-		fprintf(stderr, "Too many arguments specified.\n");
-		return false;
+	return true;
+}
+
+uint crc32(bin_t* buffer) {
+	uint crc = 0xFFFFFFFF;
+
+	for (int i = 0; i < buffer->size; i++) {
+		crc ^= ((uint)buffer->data[i] << 0x18);
+
+		for (int j = 0; j < 8; j++)
+			crc = ((crc & 0x80000000u)
+				? 0x04C11DB7u
+				: 0x00000000u
+			) ^ (crc << 1);
 	}
 
-	argc = c;
-	argv = v;
-
-	FILE* handle = fopen(argv[1], "rb");
-	if (handle == NULL) {
-		fprintf(stderr, "Failed to open input file.\n");
-		exit(1);
-	}
-
-	fseek(handle, 0, SEEK_END);
-	input.size = ftell(handle);
-
-	fseek(handle, 0, SEEK_SET);
-
-	input.data = (byte*)malloc(input.size * sizeof(byte));
-	if (input.data == NULL) {
-		fprintf(stderr, "Failed to allocate memory for input file.\n");
-
-		fclose(handle);
-		exit(1);
-	}
-
-	(void)fread(input.data, input.size, 1, handle);
-	fclose(handle);
+	return crc;
 }
 
 void print_vector_error(int i, const char* error, std::vector<byte> v) {
@@ -113,6 +117,70 @@ int verify_byte_array(int i, const std::vector<byte> values, const char* error) 
 		}
 	
 	return values.size();
+}
+
+void verify_uints(int i, uint a, uint b, const char* error) {
+	if (a != b) {
+		fprintf(stderr, "Syntax error at position %08x: mismatching %s.\n", i, error);
+		exit(1);
+	}
+}
+
+void convert();
+
+int main(int c, char** v) {
+	if (c < 2) {
+		fprintf(stderr, "No input file specified.\n");
+		return false;
+	}
+
+	if (c > 3) {
+		fprintf(stderr, "Too many arguments specified.\n");
+		return false;
+	}
+
+	argc = c;
+	argv = v;
+
+	FILE* handle = fopen(argv[1], "rb");
+	if (handle == NULL) {
+		fprintf(stderr, "Failed to open input file.\n");
+		exit(1);
+	}
+
+	fseek(handle, 0, SEEK_END);
+
+	if (!allocate_buffer(&input, ftell(handle), "INPUT")) {
+		fclose(handle);
+		exit(1);
+	}
+
+	fseek(handle, 0, SEEK_SET);
+	(void)fread(input.data, input.size, 1, handle);
+	fclose(handle);
+
+	convert();
+
+	char** output_file;
+
+	if (argc == 3) output_file = &argv[2];
+	else {
+		strcpy(&argv[1][strlen(argv[1]) - 3], default_output);
+		output_file = &argv[1];
+	}
+
+	handle = fopen(*output_file, "wb");
+	if (handle == NULL) {
+		fprintf(stderr, "Failed to open output file.\n");
+		exit(1);
+	}
+
+	fwrite(output.data, sizeof(*output.data), output.size, handle);
+	fclose(handle);
+
+	printf("Success! Saved to %s\n", *output_file);
+
+	return 0;
 }
 
 #endif
--- LPX-FirmwareTool/lpx-syxtobin/main.cpp
+++ LPX-FirmwareTool/lpx-syxtobin/main.cpp
@@ -1,3 +1,5 @@
+#define SYXTOBIN
+
 #include "common.h"
 
 uint nibbles_to_uint(int* i, int length) {
@@ -11,9 +13,7 @@ uint nibbles_to_uint(int* i, int length) {
 	return result;
 }
 
-int main(int argc, char** argv) {
-	parse_args(argc, argv);
-
+void convert() {
 	std::vector<byte> expected_types = {UPDATE_INIT};
 
 	for (int i = 0; i < input.size;) {
@@ -28,31 +28,50 @@ int main(int argc, char** argv) {
 				verify_byte(i++, LPX_FAMILY_ID, "LPX_FAMILY_ID");
 				verify_byte(i++, product_types, "PRODUCT_TYPES");
 
-				i += 3;
-
-				version = nibbles_to_uint(&i, 3);
-
-				verify_byte(i++, SYSEX_END, "SYSEX_END");
+				version = nibbles_to_uint(&i, 6);
 
 				expected_types = {UPDATE_HEADER};
 				break;
 
 			case UPDATE_HEADER:
-				printf("header");
-				return 0;
-				break;
+				i += 1;
 
-			case UPDATE_WRITE:
-				printf("write");
-				return 0;
+				verify_uints(i, version, nibbles_to_uint(&i, 6), "VERSION");
+
+				if (!allocate_buffer(&output, nibbles_to_uint(&i, 8), "OUTPUT")) exit(1);
+
+				checksum = nibbles_to_uint(&i, 8);
+
+				w = 0x20;
+				expected_types = {UPDATE_WRITE, UPDATE_FINISH};
 				break;
 
 			case UPDATE_FINISH:
-				printf("finish");
-				return 0;
+				w = 0x00;
+				expected_types = {};
+
+			case UPDATE_WRITE:
+				for (int j = 0; j < 256; j++) {
+					int shift = 6 - j % 7;
+					int target = w + j / 8;
+
+					if (target >= output.size) {
+						expected_types = {UPDATE_FINISH};
+						break;
+					}
+
+					if (j % 8 == 0) output.data[target] = 0;
+					
+					output.data[target] |= (input.data[i + j / 7] & (1 << shift)) >> shift << (7 - j % 8);
+				}
+
+				w += 0x20;
+				i += 0x25;
 				break;
 		}
+
+		verify_byte(i++, SYSEX_END, "SYSEX_END");
 	}
 
-	return 0;
+	verify_uints(input.size, checksum, crc32(&output), "CHECKSUM");
 }
