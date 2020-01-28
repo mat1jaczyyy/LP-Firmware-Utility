60dfd34a4ecd67cd3bbf55ed83d0bc8cafb71d50
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-01-28 10:58:15 +0100

    Fix portsMatch again lmao

--- web/src/logic.js
+++ web/src/logic.js
@@ -35,7 +35,7 @@ const patchFirmware = async(args) => {
   return FS.readFile("firmware/output.syx")
 }
 
-const portsMatch = (input, output) => input.ToUpperCase().Replace("IN", "").Replace("OUT", "") === output.ToUpperCase().Replace("IN", "").Replace("OUT", "");
+const portsMatch = (input, output) => input.toUpperCase().replace("IN", "").replace("OUT", "") === output.toUpperCase().replace("IN", "").replace("OUT", "");
 
 const waitForIdentification = (e, setError) => {
   if(e.data.length != 17) return;
