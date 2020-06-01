a60fc26ab990503815fc59778cf8168a56ade3b0
Brendan Allan <brendonovich@outlook.com>
2020-06-01 11:25:37 +0800

    Flip hue slider direction

--- web/src/components/ColorPicker/Hue.tsx
+++ web/src/components/ColorPicker/Hue.tsx
@@ -6,7 +6,7 @@ import React, {
   useEffect,
 } from "react";
 
-import "./index.css"
+import "./index.css";
 
 interface Props {
   width: number;
@@ -34,15 +34,15 @@ const Hue = ({
   const handleMouseMove = useCallback(
     (e) => {
       const y = typeof e.pageY === "number" ? e.pageY : e.touches[0].pageY;
-
+  
       const { top: containerTop } = container.current!.getBoundingClientRect();
 
       const top = y - (containerTop + window.pageYOffset);
 
       let h;
-      if (top < 0) h = 1;
-      else if (top > height) h = 0;
-      else h = -(top / height) + 1;
+      if (top < 0) h = 0;
+      else if (top > height) h = 1;
+      else h = top / height;
 
       setHue(h);
 
@@ -78,7 +78,7 @@ const Hue = ({
           position: "absolute",
           left: "0px",
           right: "0px",
-          top: `${-(hue * 100) + 100}%`,
+          top: `${hue * 100}%`,
         }}
       >
         <div
