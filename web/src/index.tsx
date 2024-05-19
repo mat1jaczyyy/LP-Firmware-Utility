193ef846152ab3d4cbe32dd743f56e7720faa85b
Brendan Allan <brendonovich@outlook.com>
2024-05-19 10:33:02 +0800

    tailwind directives in index.css

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'mobx-react-lite/batchingForReactDom'
+import "mobx-react-lite/batchingForReactDom";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import "./assets/main.css";
 import App from "./App";
 import { StoreContext, store } from "./store";
 
@@ -14,5 +13,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </StoreContext.Provider>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
